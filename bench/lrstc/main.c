23d7844dc7e6e8592d191633875cfdbab4c25aa4
Dichloromethane <>
2018-12-23 14:25:43 +0100

    Fixed indentation

--- bench/lrstc/main.c
+++ bench/lrstc/main.c
@@ -5,9 +5,17 @@
 
 #define N ((uint64_t) 1<<20)
 
+#ifdef _OPENMP
+#define NEFF (N*omp_get_max_threads())
+#else
+#define NEFF (N)
+#endif
+
 uint64_t ld_ref(long);
 uint64_t lrstc_inc_eh_0(long);
 uint64_t lrstc_inc_eh_1(long);
+uint64_t lrstc_inc_lock_eh_0(long);
+uint64_t lrstc_inc_lock_eh_1(long);
 uint64_t latom_inc(long);
 
 int
@@ -22,32 +30,39 @@ main(int argc, char **argv)
   printf("EH=0, no contention: %ld\n", res/N);
   res = lrstc_inc_eh_1(N);
   printf("EH=1, no contention: %ld\n", res/N);
+  res = lrstc_inc_lock_eh_0(N);
+  printf("EH=0, locking, no contention: %ld\n", res/N);
+  res = lrstc_inc_lock_eh_1(N);
+  printf("EH=1, locking, no contention: %ld\n", res/N);
   res = 0;
 #pragma omp parallel reduction(+:res)
   {
 	tmp = latom_inc(N);
 	res += tmp;
   }
-#ifdef _OPENMP
-  res = res/((uint64_t) omp_get_max_threads());
-#endif
-  printf("Atomic, high contention: %ld\n", res/N);
+  printf("Atomic, high contention: %ld\n", res/NEFF);
 #pragma omp parallel reduction(+:res)
   {
 	tmp = lrstc_inc_eh_0(N);
 	res += tmp;
   }
-#ifdef _OPENMP
-  res = res/((uint64_t) omp_get_max_threads());
-#endif
-  printf("EH=0, high contention: %ld\n", res/N);
+  printf("EH=0, high contention: %ld\n", res/NEFF);
 #pragma omp parallel reduction(+:res)
   {
 	tmp = lrstc_inc_eh_1(N);
 	res += tmp;
   }
-#ifdef _OPENMP
-  res = res/((uint64_t) omp_get_max_threads());
-#endif
-  printf("EH=1, high contention: %ld\n", res/N);
+  printf("EH=1, high contention: %ld\n", res/NEFF);
+#pragma omp parallel reduction(+:res)
+  {
+	tmp = lrstc_inc_lock_eh_0(N);
+	res += tmp;
+  }
+  printf("EH=0, locking, high contention: %ld\n", res/NEFF);
+#pragma omp parallel reduction(+:res)
+  {
+	tmp = lrstc_inc_lock_eh_1(N);
+	res += tmp;
+  }
+  printf("EH=1, locking, high contention: %ld\n", res/NEFF);
 }
