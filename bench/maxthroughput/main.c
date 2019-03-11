5d49c6d183fce2dde1faf5bbca1b621dd9a07e2d
Dichloromethane <>
2019-03-11 21:31:48 +0100

    Added a bunch of stuff

--- bench/maxthroughput/main.c
+++ bench/maxthroughput/main.c
@@ -6,6 +6,11 @@ double through_xsadddp_dr(long);
 double through_xvadddp(long);
 double through_addi(long);
 double through_clzd(long);
+double through_fused(long);
+double through_fused2(long);
+double through_li(long);
+double through_ld(long);
+double through_lddr(long);
 
 #ifndef NS
 #define NS 15
@@ -17,12 +22,17 @@ double through_clzd(long);
 int
 main(int argc, char **argv)
 {
-  double d1, d2, d3, d4, d5;
+  double d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
   d1 = through_xsadddp(N);
-  d5 = through_xsadddp_dr(N);
   d2 = through_xvadddp(N);
   d3 = through_addi(N);
   d4 = through_clzd(N);
-  printf("%8.3f %8.3f %8.3f %8.3f %8.3f\n", d1, d2, d3, d4, d5);
+  d5 = through_xsadddp_dr(N);
+  d6 = through_fused(N);
+  d7 = through_li(N);
+  d8 = through_fused2(N);
+  d9 = through_ld(N);
+  d10 = through_lddr(N);
+  printf("%8.3f %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f\n", d1, d2, d3, d4, d5, d6, d7, d8, d9, d10);
   return 0;
 }
--- bench/maxthroughput/run_tests.sh
+++ bench/maxthroughput/run_tests.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 
-printf "%8s %8s %8s %8s %8s %8s\n" "num" "xsadddp" "xvadddp" "addi" "clzd" "xsad3pdr"> out.dat
+printf "%8s %8s %8s %8s %8s %8s %8s %8s %8s %8s %8s\n" "num" "xsadddp" "xvadddp" "addi" "clzd" "xsad3pdr" "fused" "li" "fused2" "ld" "lddr"> out.dat
 
 for i in `seq 32`; do 
   printf "%8d " $i >> out.dat
   gcc -DNUM_INSTR=$i main.c through.S && ./a.out >> out.dat; 
 done
 
-rm -f a.out
+#rm -f a.out
 
-gnuplot -p -e 'plot for [col=2:6] "out.dat" using 1:col with linespoints title columnheader(col)'
+gnuplot -p -e 'plot for [col=2:11] "out.dat" using 1:col w lp t columnheader(col)'
