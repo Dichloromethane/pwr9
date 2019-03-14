7d6147b2f518f01ad28535a3a9a5d024a4f30bb2
Dichloromethane <>
2019-03-14 13:47:46 +0100

    Added single instr measuring w/ dispatch interlock

--- bench/README.md
+++
@@ -1,3 +0,0 @@
-# Benchmarks for IBM's Power9 processor
-
-Just a bunch of benchmarks for all kinds of stuff
--- bench/single_instr_measure/main.c
+++ bench/single_instr_measure/main.c
@@ -2,12 +2,17 @@
 
 long ref(void);
 long blr_malign(void);
+long blr_malign_dispatch_interlock(void);
 
 int
 main(int argc, char **argv)
 {
-  long m1, m2;
+  long m1, m2, m3;
+  ref(); // load L1I$
   m1 = ref();
+  blr_malign(); // load L1I$
   m2 = blr_malign();
-  printf("%ld, %ld\n", m1, m2);
+  blr_malign_dispatch_interlock(); // load L1I$
+  m3 = blr_malign_dispatch_interlock();
+  printf("%ld %ld %ld\n", m1, m2, m3);
 }
